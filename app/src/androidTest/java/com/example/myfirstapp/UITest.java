4ddd1f41a8beb27f5df874b39bb739e3d1c2efc1
Shaun Tseng <shaun92168@gmail.com>
2020-09-16 12:41:17 -0700

    first commit

---
+++ app/src/androidTest/java/com/example/myfirstapp/UITest.java
@@ -0,0 +1,28 @@
+package com.example.myfirstapp;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class UITest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void listGoesOverTheFold() {
+        onView(withId(R.id.editText)).perform(typeText("This is a Test"), closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText("This is a Test")));
+    }
+
+}
