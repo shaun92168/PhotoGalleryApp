e7f80e335c5e6f2e1cc1e787f16becd6f6b9f5bb
mahoushisaber <mahoushisaber@gmail.com>
2020-09-18 17:53:12 -0700

    Added external file storage
    Pictures are stored in the Root/PohotoGallery folder with its name start with timestamp.

--- app/src/main/java/com/example/photogalleryapp/MainActivity.java
+++ app/src/main/java/com/example/photogalleryapp/MainActivity.java
@@ -6,16 +6,21 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,9 +28,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@SuppressWarnings("deprecation")
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_CODE = 1001;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     TextView dateTextView;
     EditText latEditText, longiEditText, captionEditText;
     Button leftBtn, rightBtn, snapBtn, uploadBtn;
@@ -57,9 +74,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-
-
-
     }
 
     private void askPermission() {
@@ -72,25 +86,108 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(permission, PERMISSION_CODE);
             } else {
                 // Already granted permission
-                openCamera();
+                dispatchTakePictureIntent();
             }
         }
         else{
             // OS < marshmallow
-            openCamera();
+            dispatchTakePictureIntent();
         }
     }
 
-    private void openCamera() {
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "New Picture");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From Camera");
-        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        // Opens Camera Intent
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
-        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            // Better resolution than Bitmap
+            File f = new File(currentPhotoPath);
+            image.setImageURI(Uri.fromFile(f));
+            // Location stored
+            Log.d("tag", "Absolute Url of Image is: "+ Uri.fromFile(f));
+
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(f);
+            mediaScanIntent.setData(contentUri);
+            this.sendBroadcast(mediaScanIntent);
+        }
     }
+
+    // Save image with timestamp caption
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        // Can append geoloction to file name for searching
+        String imageFileName = timeStamp + "_";
+
+        File storageDir = Environment.getExternalStorageDirectory();
+        File dir = new File(storageDir.getAbsolutePath() + "/PhotoGallery");
+        dir.mkdirs();
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                dir      /* directory */
+        );
+
+        // Saving to gallery instead
+        //context.getExternalFilesDirs(null);
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        //        File image = File.createTempFile(
+        //                    imageFileName,  /* prefix */
+        //                    ".jpg",         /* suffix */
+        //                    storageDir      /* directory */
+        //            );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+
+        // Check SD card's paths
+        if (isSdCardExist()){
+        Log.d("tag", "SD card exist");
+        Log.d("tag", getSdCardPath());
+        Log.d("tag", getDefaultFilePath());
+        }
+
+        return image;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Check SD card exist or not
+    public static boolean isSdCardExist() {
+        return Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED);
+    }
+    // Get the root of SD card
+    public static String getSdCardPath() {
+        boolean exist = isSdCardExist();
+        String sdpath = "";
+        if (exist) {
+            sdpath = Environment.getExternalStorageDirectory()
+                    .getAbsolutePath();
+        } else {
+            sdpath = "Not Applicable";
+        }
+        return sdpath;
+    }
+
+    // Grant the default path for storing
+    public static String getDefaultFilePath() {
+        String filepath = "";
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "abc.txt");
+        if (file.exists()) {
+            filepath = file.getAbsolutePath();
+        } else {
+            filepath = "Not Applicable";
+        }
+        return filepath;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
     // Handling permission result
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
@@ -98,7 +195,7 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode){
             case PERMISSION_CODE:{
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    openCamera();
+                    dispatchTakePictureIntent();
                 } else {
                     Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
                 }
@@ -108,22 +205,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     // Calling camera app with another intent
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, IMAGE_CAPTURE_CODE);
+            }
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            image.setImageURI(image_uri);
-        }
+
+    private void setPic() {
+        // Get the dimensions of the View
+        int targetW = image.getWidth();
+        int targetH = image.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        image.setImageBitmap(bitmap);
+        Toast.makeText(MainActivity.this, "Saved memory by decoding", Toast.LENGTH_SHORT).show();
     }
 
     public void sendMessage(View view) {
